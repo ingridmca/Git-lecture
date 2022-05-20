d1b4150ac73149da7e0db9dc78b8f34793af2ddd
Ingrid Almeida <ingridmcalmeida@gmail.com>
2022-05-20 15:28:21 +0200

    Git lecture continue

---
+++ gitLecture.js
@@ -0,0 +1,19 @@
+// What is GIT for?
+// VERSION CONTROL AND COLABORATING
+
+//Get Comands
+
+// git init  (just once)
+//start at master branch a branch is a diferent version of the same code
+//change the master to main  using  < git branch -m main >, or other name
+//<git status>
+//<git add .>
+//<git commit -m>
+// creat an repository on GIT and get this option
+//git remote add origin git@github.com:ingridmca/Git-lecture.git (just once)
+//git branch -M main
+//git push -u origin main
+
+//Get Flow
+
+//
